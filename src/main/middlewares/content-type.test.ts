695222ee646bcb02eb6afc9caa56a908b3f9bb48
roberttorres <robertorres@gmail.com>
2021-08-13 11:57:22 -0300

    test: ensure mongodb reconnect if client is down

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 
-describe('COontent Type Middleware', () => {
+describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {  
     app.get('/test_content_type', (req, res) => {
       res.send('')

b9c881b4447c6805e5107470ae276ad31afc508d
roberttorres <robertorres@gmail.com>
2021-08-18 10:11:14 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,16 +1,7 @@
 import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
 import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
-import { EmailValidator, HttpRequest, Authentication } from './login-protocols'
+import { HttpRequest, Authentication, Validation } from './login-protocols'
 import { LoginController } from './login'
-
-const makeEmailValidator = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorStub()
-}
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
@@ -21,20 +12,29 @@ const makeAuthentication = (): Authentication => {
   return new AuthenticationStub()
 }
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }  
+      return new ValidationStub()    
+}   
+
 interface SutTypes {
   sut: LoginController
-  emailValidatorStub: EmailValidator
   authenticationStub: Authentication
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
-  const emailValidatorStub = makeEmailValidator()
   const authenticationStub = makeAuthentication()
-  const sut = new LoginController(emailValidatorStub, authenticationStub)
+  const validationStub = makeValidation()
+  const sut = new LoginController(authenticationStub, validationStub)
   return {      
     sut,
-    emailValidatorStub,
-    authenticationStub
+    authenticationStub,
+    validationStub
   }  
 }
 
@@ -47,51 +47,7 @@ const makeFakeRequest = (): HttpRequest => ({
 
 
 describe('Login Controller', () => {
-    test('Should return 400 if no email is provided', async () => {  
-      const { sut } = makeSut()
-      const httpRequest = {
-        body: {
-          password: 'any_password'
-        }
-      }
-      const httpResponse = await sut.handle(httpRequest)
-      expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-    })      
-
-    test('Should return 400 if no password is provided', async () => {  
-      const { sut } = makeSut()
-      const httpRequest = {
-        body: {
-          email: 'any_email@mail.com'
-        }
-      }
-      const httpResponse = await sut.handle(httpRequest)
-      expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-    }) 
-
-    test('Should return 400 if an invalid email is provided', async () => {  
-      const { sut, emailValidatorStub } = makeSut()
-      jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-      const httpResponse = await sut.handle(makeFakeRequest())
-      expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
-    }) 
-    
-    test('Should call EmailValidator with correct email', async () => {  
-      const { sut, emailValidatorStub } = makeSut()
-      const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')      
-      await sut.handle(makeFakeRequest())
-      expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
-    }) 
-
-    test('Should return 500 if EmailValidator throws', async () => {  
-      const { sut, emailValidatorStub } = makeSut()
-      jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
-        throw new Error()
-      })
-      const httpResponse = await sut.handle(makeFakeRequest())
-      expect(httpResponse).toEqual(serverError(new Error()))
-    }) 
-      
+     
     test('Should call Authentication with correct values', async () => {  
       const { sut, authenticationStub } = makeSut()
       jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))      
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,9 +1,9 @@
-import { HttpResponse, HttpRequest, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { HttpResponse, HttpRequest, Controller, AddAccount } from './signup-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 import { Validation } from '../../helpers/validators/validation'
  
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
+  
   private readonly addAccount: AddAccount
   private readonly validation: Validation
 

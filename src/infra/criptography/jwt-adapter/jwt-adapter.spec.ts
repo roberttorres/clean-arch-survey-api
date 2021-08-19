c07681142bb6db01137463da934b33d1302e5ba3
roberttorres <robertorres@gmail.com>
2021-08-19 14:49:14 -0300

    test: ensure JwtAdapter returns a token when sign succeed

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {    
     const sut = new JwtAdapter('secret') 
@@ -8,4 +14,15 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id')   
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id'} , 'secret')     
   })
+
+  test('Should return a token on sign success', async () => {    
+    const sut = new JwtAdapter('secret') 
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
+  })
 })  
+
+/* Neste arq de teste não é preciso saber como o jsonwebtoken funciona p/ conseguir testar.
+A gente faz o mock dele e só quer garantir que o resultado do mock (jest.mock) 
+seja o mesmo retornado pela sut -> (expect(accessToken).toBe('any_token'))*/
+ 

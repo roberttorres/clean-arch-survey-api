c52f1c27971b9820b0e46a0cfec750dd3128bdab
roberttorres <robertorres@gmail.com>
2021-08-20 10:25:58 -0300

    refactor: rename update with updateAccessToken

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -26,14 +26,12 @@ describe('Jwt Adapter', () => {
   })
 
   test('Should throw if sign throws', async () => {    
-    const sut = makeSut() 
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    const promise = await sut.encrypt('any_id')   
+    const sut = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {throw new Error()})
+    const promise = sut.encrypt('any_id')   
     await expect(promise).rejects.toThrow()
-  })
-  
+  })  
+
 })  
 
 /* Neste arq de teste não é preciso saber como o jsonwebtoken funciona p/ conseguir testar.

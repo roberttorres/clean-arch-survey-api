60b3dc93020d9f8f0142b575448a687f5d5393d5
Robert Torres <robertorres@gmail.com>
2021-08-27 11:13:38 -0300

    test: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,8 +1,9 @@
 import { SignUpController } from './signup-controller'
-import { MissingParamError, ServerError } from '../../errors'
+import { EmailInUseError, MissingParamError, ServerError } from '../../errors'
 import {  AccountModel, AddAccount, AddAccountModel, Validation, Authentication, AuthenticationModel } from './signup-controller-protocols'
 import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
+import { ok, serverError, badRequest, forbidden } from '../../helpers/http/http-helper'
+import { resolve } from 'path/posix'
  
 
 const makeAddAccount = (): AddAccount => {
@@ -109,6 +110,13 @@ describe('SignUp Controller', () => {
       password: 'valid_password',
     })
   }) 
+
+  test('Should return 403 if AddAccount returns null', async () => {
+    const { sut, addAccountStub } = makeSut()       
+    jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(makeFakeRequest())    
+    expect(httpResponse).toEqual(forbidden(new EmailInUseError()))    
+  })
   
   test('Should return 200 if valid data is provided', async () => {
     const { sut } = makeSut()     

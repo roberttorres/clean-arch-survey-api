a06dd5ac5f78619f014a6d1f4227ec0491a87119
Robert Torres <robertorres@gmail.com>
2021-08-30 23:38:39 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -12,4 +12,9 @@ export class JwtAdapter implements Encrypter {
     const accessToken = await jwt.sign({ id: value }, this.secret)
     return accessToken
   }
+
+  async decrypt (value: string): Promise<string> {
+    await jwt.verify(value, this.secret)
+    return null
+  }
 }

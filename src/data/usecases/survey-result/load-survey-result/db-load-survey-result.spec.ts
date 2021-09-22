15048fd5ec6e5b698ccf59ef1b8547b73da460b9
Robert Torres <robertorres@gmail.com>
2021-09-22 11:14:34 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,18 +1,37 @@
 import { LoadSurveyResultRepository } from "@/data/protocols/db/survey-result/load-survey-result-repository"
 import { SurveyResultModel } from "@/domain/models/survey-result"
 import { mockSurveyResultModel } from "@/domain/test"
+import { LoadSurveyResult } from "@/domain/usecases/survey-result/load-survey-result"
 import { DbLoadSurveyById } from "../../survey/load-survey-by-id/db-load-survey-by-id"
 import { DbLoadSurveyResult } from "./db-load-survey-result"
 
+type SutTypes = {
+  sut: DbLoadSurveyResult,
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }    
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {  
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return { 
+    sut,
+    loadSurveyResultRepositoryStub
+  }  
+}
+
+
 describe('DbLoadSurveyResult UseCase', () => {
-  test('Should call LoadSurveyResultRepository', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-        return Promise.resolve(mockSurveyResultModel())
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  test('Should call LoadSurveyResultRepository', async () => {    
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
